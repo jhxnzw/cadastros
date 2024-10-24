b19b1a9e6e30926ddb8d926c319968ce78b64d92
jhxnzw <144861154+jhxnzw@users.noreply.github.com>
2024-10-24 08:44:50 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbhost = 'localhost';
+$dbUsername = 'Jhxnzw';
+$dbPassword = 'Jhxnzw1$';
+$dbName = 'cadastros_site';
+
+$conexao = new mysqli($dbhost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexao->connect_errno)
+//{
+//    echo "Erro";    
+//}
+//else 
+//{
+//  echo "Conectado";
+//}
+//
+?>

5edebd42dbcedb22c3db429f394904ce4b20b8d1
jhxnzw <144861154+jhxnzw@users.noreply.github.com>
2024-10-28 08:23:50 -0300

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `dados` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `idade` int NOT NULL,
+  `CPF` varchar(14) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
